a549c3fbbad5f5c5e207d97b1685f739f50e843b
mengxuetong <mengxt@foxmail.com>
2015-03-09 16:16:11 +0800

    all

--- app.js
+++ app.js
@@ -1,60 +1,24 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var url = require('url');
+var bodyParser= require('body-parser');
+var multer = require('multer');
+var util = require('util');
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+var fs = require('fs');
+app.use(express.static(__dirname));
+app.use(multer({dest:"./"}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/',function(req,res){
+    res.sendfile('./index.html');
+});
+//app.get('/sock',function(req,res){
+//    res.sendFile('sock.html',{root:__dirname})
+//});
+app.post('/reg',function(req,res){
+    var urlObj = url.parse(req.url,true);
+    fs.createReadStream(req.files.avatar.path).pipe(res); //读取数据流
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
+app.listen(8333);
---
+++ sock.js
@@ -0,0 +1,22 @@
+/**
+ * Created by MENG on 2015/3/9.
+ */
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname));
+app.get('/sock',function(req,res){
+    res.sendfile('./sock.html');
+})
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection',function(socket){
+    socket.send('服务端的消息');
+    socket.on('message',function(data){
+        console.log(data);
+        socket.emit('messsage',"你更"+data);
+    });
+    console.log('connected');
+});
+
+server.listen(3333);
