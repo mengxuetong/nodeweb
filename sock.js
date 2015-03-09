ad84fca91137f01951927a0e9e3e36da0cde32bc
mengxuetong <mengxt@foxmail.com>
2015-03-09 17:50:38 +0800

    update

--- sock.js
+++ sock.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
 app.get('/sock',function(req,res){
+    //req.setRequestHeader("Access-Control-Allow-Origin: http://localhost");
     res.sendfile('./sock.html');
 })
 var server = require('http').createServer(app);
@@ -14,9 +15,9 @@ io.on('connection',function(socket){
     socket.send('服务端的消息');
     socket.on('message',function(data){
         console.log(data);
-        socket.emit('messsage',"你更"+data);
+        //socket.emit('messsage',"你更"+data);
     });
     console.log('connected');
 });
 
-server.listen(3333);
+server.listen(8888);
